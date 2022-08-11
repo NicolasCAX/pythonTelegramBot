d2e1fe6330bc3e46449faa86bf8764db322c5786
Nicolas Castillo <41174211+NicolasCAX@users.noreply.github.com>
2022-08-11 19:52:01 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,17 +4,14 @@ import os
 import requests
 import json
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Hello {update.effective_user.first_name}\nI am a sample Telegram bot made with PythonTelegramBot y el price de Bitcoin es de $!' + getBtc())
-
 def getBtc():
-    try:
       response = requests.get("https://www.bitstamp.net/api/v2/ticker/btcusd")
       response = response.json()
       return str("{:,.2f}".format(float(response["last"])))
-    except:
-      return getError()
-   
+    
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Hello {update.effective_user.first_name}\nI am a sample Telegram bot made with PythonTelegramBot y el price de Bitcoin es de ${getBtc()}!')
+
     
 updater = Updater(os.environ.get("TOKEN"), use_context=True)
 
